8d850efbff37aace1734879858ff38f2f14b21c1
psahoo-mirp <psahoo-mirp@mit.edu>
2017-05-31 11:09:21 +0530

    adding file arindam

---
+++ arindam.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{int a,b,c,d;
+cout<<"enter four integer";
+cin>>a>>b>>c>>d;
+if(a>b && a>c && a>>d)
+{cout<<"a is max";
+}
+else if (b>a && b>c && b>d)
+{cout<<"b is max";
+}
+else if ( c>a && c>b && c>d)
+{cout<<"c is max";
+}
+else 
+{cout<<"d is max";
+} 
+}
